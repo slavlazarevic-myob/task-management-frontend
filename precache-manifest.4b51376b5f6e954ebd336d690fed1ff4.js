a679056104c69c6254ad36185ccb76e14eda7908
slavlazarevic-myob <slavlazarevic-myob@users.noreply.github.com>
2021-06-13 11:28:19 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.4b51376b5f6e954ebd336d690fed1ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa8b8f66abe5452b2ebe7a15b625b0a7",
+    "url": "https://slavlazarevic-myob.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "fe6d073c78e6ae41881d",
+    "url": "https://slavlazarevic-myob.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "2a422150e88959b51a25",
+    "url": "https://slavlazarevic-myob.github.io/task-management-frontend/static/js/2.2e486377.chunk.js"
+  },
+  {
+    "revision": "fe6d073c78e6ae41881d",
+    "url": "https://slavlazarevic-myob.github.io/task-management-frontend/static/js/main.b9ab1faa.chunk.js"
+  },
+  {
+    "revision": "cdb4628ad33074baa5de",
+    "url": "https://slavlazarevic-myob.github.io/task-management-frontend/static/js/runtime~main.bc5133eb.js"
+  }
+]);
